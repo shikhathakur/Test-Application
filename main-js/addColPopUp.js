3004f9f755d69bd6f5e632105b6131f58359f3b6
shikhathakur <sthakur@innominds.com>
2016-01-18 10:28:23 +0530

    Update addColPopUp.js

--- main-js/addColPopUp.js
+++ main-js/addColPopUp.js
@@ -1,32 +1,42 @@
 //Function To Display Popup
 function fnAddColumn() {
-document.getElementById('abc').style.display = "block";
+    document.getElementById('abc').style.display = "block";
 }
 //Function to Hide Popup
-function closePopUp(){
-document.getElementById('abc').style.display = "none";
+function closePopUp() {
+    document.getElementById('abc').style.display = "none";
+}
+if (document.getElementById('columnName') != null) {
+    var scolumnName = document.getElementById('columnName').text;
 }
 
-//Pop-Up for adding columns
-function fnAddColumnPopup(){
-	var scolumnName = document.getElementById('columnName').value;
-    localStorage.setItem('colName',scolumnName);
+if (document.getElementById('datatype') != null) {
     var scolumnType = document.getElementById('datatype');
     var sType = scolumnType.options[scolumnType.selectedIndex].text;
-    localStorage.setItem('colType',sType);
-    var sEditable=document.getElementById('editable');
-	var sEdit = sEditable.options[sEditable.selectedIndex].text;
-	localStorage.setItem('edit',sEdit);
-    (function(){
-        var table = document.getElementById("tableColData");
-        var row = table.insertRow(1);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
-        cell1.innerHTML = "<input type='checkbox'>";
-        cell2.innerHTML = localStorage.getItem('colName');
-        cell3.innerHTML = localStorage.getItem('colType');
-        cell4.innerHTML = localStorage.getItem('edit');
-    })();
+}
+
+if (document.getElementById('editable') != null) {
+    var sEditable = document.getElementById('editable');
+    var sEdit = sEditable.options[sEditable.selectedIndex].text;
+}
+
+function fnAddColumnPopup() {
+    var oTable = document.getElementsByTagName('table');
+    var otRow = document.createElement('tr');
+    oTable[0].appendChild(otRow);
+    var otData1 = document.createElement('td');
+    otRow.appendChild(otData1);
+    var oInput = document.createElement('input');
+    otData1.appendChild(oInput);
+    oInput.setAttribute('type', 'checkbox');
+    oInput.setAttribute('value', 'true');
+    var otData2 = document.createElement('td');
+    otRow.appendChild(otData2);
+    otData2 = scol;
+    var otData3 = document.createElement('td');
+    otRow.appendChild(otData3);
+    otData3 = sType;
+    var otData4 = document.createElement('td');
+    otRow.appendChild(otData4);
+    otData4 = sEdit;
 }
